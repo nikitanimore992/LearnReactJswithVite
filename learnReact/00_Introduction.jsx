17b70c129946d1b4c422133ac6dbd9d1a13104dd
nikita nimore <992nikitanimore@gmail.com>
2024-11-19 18:23:35 +0530

    create a new intro page in react js

---
+++ learnReact/00_Introduction.jsx
@@ -0,0 +1,21 @@
+
+
+/*
+
+React JS is a javascript library originally developed by Facebook. 
+It helps in building highly engaging single-page web applications.
+React JS helps in breaking down complex UI into simpler components. 
+React JS has a high demand in the field of Web development.
+
+extention you need to downlowd
+        Auto Rename Tage
+        Babel JavaScript for js also
+        Bracket Pair Color DLW
+        Bracket Pair Colorization
+        ES7 React/Redux/GraphQL/React-Native snippets
+        Live Server
+        One Dark Pro
+        Prettier - Code Formatter
+        StandardJS - JavaScript Standard Style
+        Thunder Client
+*/
